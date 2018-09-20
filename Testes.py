6249ad0c9c3fd6205a7fe42107fb84ade91249c0
Brockzera <brochj@gmail.com>
2018-09-19 21:44:56 -0300

    Brock

--- Testes.py
+++ Testes.py
@@ -18,10 +18,10 @@ height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))    # Retorna a altura do video
 length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))     # Retorna a quantidade de frames
 
 kernel1 = np.ones((3,3), np.uint8) # Matriz (3,3) com 1 em seus valores -- Usa na funcao de erode
-kernel2 = np.ones((7,7), np.uint8) # # Matriz (8,8) com 1 em seus valores -- Usa na funcao de dilate
+kernel2 = np.ones((8,8), np.uint8) # # Matriz (8,8) com 1 em seus valores -- Usa na funcao de dilate
 kernel3 = np.ones((3,3), np.uint8) # # Matriz (8,8) com 1 em seus valores -- Usa na funcao de dilate
 
-RESIZE_RATIO = 0.35 # U
+RESIZE_RATIO = 0.35 # 
 
 
 bgsMOG = cv2.createBackgroundSubtractorMOG2(history=10, varThreshold = 50, detectShadows=0)
@@ -44,14 +44,14 @@ while(True):
     
     if ret == True:
         # Cria a máscara
-        fgmask = bgsMOG.apply(frameGray, None, -1)
+        fgmask = bgsMOG.apply(frameGray, None, 0.01)
         erodedmask = cv2.erode(fgmask, kernel1 ,iterations=1) # usa pra tirar os pixels isolados (ruídos)
         dilatedmask = cv2.dilate(erodedmask, kernel2 ,iterations=1) # usa para evidenciar o objeto em movimento
 #        erodedmask = cv2.erode(fgmask, kernel3 ,iterations=1) # usa pra tirar os pixels isolados (ruídos)
         # Fim da máscara
         _, contours, hierarchy = cv2.findContours(dilatedmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         
-#        frame1 = cv2.drawContours(frame, contours, -1, (0,255,0),3)
+#        outputFrame = cv2.drawContours(frame, contours, -1, (0,255,0),3)
         
         
         try: hierarchy = hierarchy[0]
@@ -60,7 +60,9 @@ while(True):
         for contour, hier in zip(contours, hierarchy):
             (x, y, w, h) = cv2.boundingRect(contour)
 
-            if w < 10 and h < 10:
+            if w < 60 and h < 60:
+                continue
+            if h < 2*w:
                 continue
 
             center = (int(x + w/2), int(y + h/2))
@@ -69,11 +71,21 @@ while(True):
                 continue
 
 				# Optionally draw the rectangle around the blob on the frame that we'll show in a UI later
-            frame1 = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            outputFrame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+        outputFrame=  cv2.putText(frame, 'frame: {}'.format(frameCount), (5,375), cv2.FONT_HERSHEY_SIMPLEX, .5, (255,255,255), 2)
+        
+        if frameCount >= 71 and frameCount <= 111:
+            outputFrame=  cv2.putText(frame, '56.65', (100,375), cv2.FONT_HERSHEY_SIMPLEX, .5, (255,255,255), 2)
+
+        if frameCount == 600: #fecha o video
+            break
         
         
-        cv2.imshow('frame1', dilatedmask)
-        cv2.imshow('frame', frame1)
+        
+        cv2.imshow('erodedmask',erodedmask)
+        cv2.imshow('dilatedmask', dilatedmask)
+        cv2.imshow('outputFrame', outputFrame)
         
         frameCount = frameCount + 1    # Conta a quantidade de Frames
         
---
+++ xmlreader.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 19 21:03:24 2018
+
+@author: broch
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 17 13:24:34 2018
+
+@author: broch
+"""
+
+import cv2
+#import numpy as np
+#import os
+#import time
+#import uuid
+#import math
+
+cap = cv2.VideoCapture("../video01.avi")
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))      # Retorna a largura do video
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))    # Retorna a altura do video
+length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))     # Retorna a quantidade de frames
+
+RESIZE_RATIO = 0.35 # U
+frameCount = 0
+
+def get_frame():
+	" Grabs a frame from the video vcture and resizes it. "
+	ret, frame = cap.read()
+	if ret:
+		(h, w) = frame.shape[:2]
+		frame = cv2.resize(frame, (int(w * RESIZE_RATIO), int(h * RESIZE_RATIO)), interpolation=cv2.INTER_CUBIC)
+	return ret, frame
+
+while(True):
+    ret , frame = get_frame()
+    
+    if ret == True:
+#        Coloca o codigo AQUI 
+        
+        
+        
+        
+        frame =  cv2.putText(frame, 'frame: {}'.format(frameCount), (5,375), cv2.FONT_HERSHEY_SIMPLEX, .5, (255,255,255), 2)
+
+        
+        
+        if frameCount >= 71 and frameCount <= 111:
+            outputFrame=  cv2.putText(frame, '56.65', (100,375), cv2.FONT_HERSHEY_SIMPLEX, .5, (255,255,255), 2)
+        
+        if frameCount == 600: #fecha o video
+            break
+        
+        cv2.imshow('frame', frame)
+        
+        frameCount += 1    # Conta a quantidade de Frames
+        
+         
+        if cv2.waitKey(1) & 0xFF == ord('q'): #Pressiona a tecla Q para fechar o video
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
