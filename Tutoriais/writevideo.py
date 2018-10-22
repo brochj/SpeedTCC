865f5956de7193aedec209253e57e4462b076d50
Brockzera <brochj@gmail.com>
2018-10-22 17:22:04 -0300

    Write Video 2

--- Tutoriais/writevideo.py
+++ Tutoriais/writevideo.py
@@ -1,52 +1,43 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 22 16:44:06 2018
-
-@author: broch
-"""
-
 import cv2
- 
 # Create a VideoCapture object
-cap = cv2.VideoCapture('../Dataset/video1.avi')
+cap = cv2.VideoCapture('../../Dataset/video1.avi')
  
-
-frame_width = 1920
-frame_height = 1080
-# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-out = cv2.VideoWriter('out.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 25, (frame_width,frame_height))
- 
+FPS = 25
+WIDTH = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # Retorna a largura do video
+HEIGHT = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # Retorna a largura do video
 frameCount = 0
+# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
+out = cv2.VideoWriter('out.avi',cv2.VideoWriter_fourcc('M','J','P','G'), FPS, (WIDTH,HEIGHT))
+ 
 while(True):
   ret, frame = cap.read()
- 
   if ret == True: 
-     
+  
     # Write the frame into the file 'output.avi'
     if frameCount > 100 and frameCount < 200:
-        cv2.putText(frame, 'frame: {}'.format(frameCount),(14, 20), 0, .65, (255,255,255), 2)
-        out.write(frame)
+        cv2.putText(frame, 'frame: {}'.format(frameCount),(14, 25), 0, 1, (255,255,255), 2)
+        out.write(frame)  # Salva o Frame
         
     if frameCount > 500 and frameCount < 600:
-        cv2.putText(frame, 'frame: {}'.format(frameCount),(14, 20), 0, .65, (255,255,255), 2)
-        out.write(frame)
+        
+        cv2.putText(frame, 'frame: {}'.format(frameCount),(14, 25), 0, 1, (255,255,255), 2)
+        out.write(frame) # Salva o Frame
     
-#    cv2.putText(frame, 'frame: {}'.format(frameCount),(14, 20), 0, .65, (255,255,255), 2)
+    
+    
+    cv2.putText(frame, 'frame: {}'.format(frameCount),(14, 25), 0, 1, (255,255,255), 2)
     # Display the resulting frame    
     cv2.imshow('frame',frame)
         
     frameCount += 1
     # Press Q on keyboard to stop recording
     if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
- 
+      break 
   # Break the loop
   else:
     break 
  
-# When everything done, release the video capture and video write objects
 cap.release()
 out.release()
- 
-# Closes all the frames
 cv2.destroyAllWindows() 
